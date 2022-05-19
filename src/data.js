8b8484fd334381710d8614492812a58328b8af5c
Ranirisoa <ranirisoajames@gmail.com>
2022-05-19 08:22:55 +0200

    share code on github

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name:"mechant chat",
+            image:'/images/img1.jpg',
+            price:120,
+            brand:'mouleu',
+            rating:'4.5',
+            numReviews:10,
+            description:'cet chat est tres mechant'
+        },
+        {
+            _id:'2',
+            name:"kitty",
+            image:'/images/img2.jpg',
+            price:60,
+            brand:'cute',
+            rating:'4.5',
+            numReviews:9,
+            description:'cet chat est tres cute'
+        },
+        {
+            _id:'3',
+            name:"lumy",
+            image:'/images/img3.jpg',
+            price:90,
+            brand:'kitilove',
+            rating:'5',
+            numReviews:12,
+            description:'cet chat est une amour'
+        },
+        {
+            _id:'4',
+            name:"nounou",
+            image:'/images/img4.jpg',
+            price:100,
+            brand:'noumou',
+            rating:'3',
+            numReviews:13,
+            description:'cet chat est tres nounours'
+        },
+        {
+            _id:'5',
+            name:"miaou",
+            image:'/images/img5.jpg',
+            price:150,
+            brand:'noumi',
+            rating:'5',
+            numReviews:7,
+            description:'cet chat est tres kiutu'
+        },
+        {
+            _id:'6',
+            name:"abella",
+            image:'/images/img6.jpg',
+            price:220,
+            brand:'sexy',
+            rating:'5',
+            numReviews:21,
+            description:'cet chat est tres sexy'
+        },
+    ]
+}
+
+export default data;
